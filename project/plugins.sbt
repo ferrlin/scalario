c4a89e673c3d1aae2c95e79854fb10e9afd25c3c
John Ferrolino <john@ferrl.in>
2014-12-12 01:08:26 +0800

    adding scalastyle plugin

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,3 +5,7 @@ resolvers += Classpaths.typesafeResolver
 addSbtPlugin("com.typesafe.sbt" % "sbt-start-script" % "0.10.0")
 
 addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.2")
+
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.6.0")
+
+resolvers += "sonatype-releases" at "https://oss.sonatype.org/content/repositories/releases/"
--- src/main/scala/ScalariverCan.scala
+++ src/main/scala/ScalariverCan.scala
@@ -1,3 +1,24 @@
+/* Copyright (c) 2014 John Ferrolino
+
+The MIT license
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.*/
 package org.scalariver
 
 import akka.actor.{ ActorSystem, Props }
@@ -7,8 +28,7 @@ import spray.can.Http
 object Boot extends App {
 
   implicit val river = ActorSystem("Scalariver")
-
-  val handler = river.actorOf(Props[ScalariverHandler], name = "scalariver")
+  val handler = river.actorOf(ScalariverHandler.props, name = "scalariver")
 
   import com.typesafe.config._
   import util.Properties
@@ -19,11 +39,15 @@ object Boot extends App {
   IO(Http) ! Http.Bind(handler, interface = server, port = serverPort)
 }
 
-import akka.actor.{ Actor, ActorLogging }
+import akka.actor.{ Actor, ActorLogging, ActorRefFactory }
 import spray.http._
 import scala.concurrent.duration._
 import akka.util.Timeout
 import scala.util.{ Try, Success, Failure }
+
+object ScalariverHandler {
+  def props: Props = Props[ScalariverHandler]
+}
 
 /**
  * Handler Actor registered to spray-can for formatting services.
@@ -56,7 +80,7 @@ object FormattingService {
   val SCALA_VERSION = "scalaVersion"
   val INDENT_LEVEL = "initialIndentLevel"
 
-  def formatPreferences(implicit params: Map[String, String]) =
+  def formatPreferences(implicit params: Map[String, String]): Map[PreferenceDescriptor[_], Any] =
     AllPreferences.preferencesByKey map {
       case (key, descriptor) ⇒ {
         val setting = descriptor match {
